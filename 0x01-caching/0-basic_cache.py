4076874037d88d825a2091b48abe2a6cdbe258a3
roxxe34 <hamzafarissi7@gmail.com>
2024-07-24 11:40:27 +0100

    chore: Add basic and FIFO cache implementations

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key
+        """
+        if key and item:
+            self.cache_data[key] = item
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""FIFO"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Assign the item value to the key in self.cache_data.
+        If the number of items exceeds BaseCaching.MAX_ITEMS,
+        discard the first item (FIFO algorithm).
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            self.cache_data.pop(first_key)
+            print(f"DISCARD: {first_key}\n")
+
+    def get(self, key):
+        """
+        Return the value associated with the key in self.cache_data.
+        If key is None or doesn't exist in self.cache_data, return None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
