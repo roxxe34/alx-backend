34faee3f716cf4a55ab342a9a6b20a4cd3a51e3d
roxxe34 <hamzafarissi7@gmail.com>
2024-08-26 09:27:23 +0100

    chore: Add i18n support to Flask app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """return the best match for supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route("/")
+def hello():
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
